545f4a24df957829261671844eb269cf47cad8a4
bill fero <billfero@gmail.com>
2017-03-09 22:56:36 +0000

    impl roll dice button - update dice img and active players current score

--- js/app.js
+++ js/app.js
@@ -11,13 +11,25 @@ GAME RULES:
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Page loaded');
 
-	let scores, roundScore, activePlayer, dice;
-
+	let scores, roundScore, activePlayer;
+	let dice = document.querySelector('.dice');
+	
 	scores = [0,0];
 	roundScore = 0;
 	activePlayer = 0;
-	dice = Math.floor(Math.random() * 6) + 1;
 	
-	document.querySelector(`#current-${activePlayer}`).textContent = dice;
+	dice.style.display = 'none'; // hide the dice until it's rolled
+
+	// add event listener to the roll dice btn
+	document.querySelector('.btn-roll').addEventListener('click', rollDice);
+	function rollDice(e) {
+		dice.style.display = 'block'; // display the dice
+		// roll the dice and display the result
+		let rolled = Math.floor(Math.random() * 6) + 1;
+		dice.src = `./imgs/dice-${rolled}.png`; // update the dice img to reflect num rolled
+		
+		// update the activePlayer's current score when the dice is rolled
+		document.querySelector(`#current-${activePlayer}`).textContent = rolled;
+	}
 	
 });
