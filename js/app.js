82011fa0a0891650d29b4930d7d4d3ed6e74c2cd
bill fero <billfero@gmail.com>
2017-03-10 10:29:42 +0000

    update global scores. Refactor rollDice callback, move player switching code to separate function

--- js/app.js
+++ js/app.js
@@ -10,17 +10,16 @@ GAME RULES:
 */
 document.addEventListener('DOMContentLoaded', function () {
 
-	let scores, roundScore, activePlayer;
+	let scores, currentTotal, activePlayer;
 	let dice = document.querySelector('.dice');
-	let score0 = document.getElementById('score-0');
-	let score1 = document.getElementById('score-1');
 	let current0 = document.getElementById('current-0');
 	let current1 = document.getElementById('current-1');
 	
+	
 	// init the app
-	score0.textContent = score1.textContent = current0.textContent = current1.textContent = '0';
+	current0.textContent = current1.textContent = '0';
 	scores = [0,0];
-	roundScore = 0;
+	currentTotal = 0;
 	activePlayer = 0;
 	
 	dice.style.display = 'none'; // hide the dice until it's rolled
@@ -36,20 +35,40 @@ document.addEventListener('DOMContentLoaded', function () {
 		// 2. update the roundScore if the num rolled is greater than 1
 		// and update the player's current score
 		if(rolled !== 1) {
-			roundScore += rolled;
-			//activePlayerScore.textContent = roundScore;
+			currentTotal += rolled;
 		} else {
-			// reset the players current score and switch to the other player
-			roundScore = 0;
-			activePlayer = (activePlayer === 0)? 1 : 0;
-			
-			// reset the current scores & update ui
-			current0.textContent = current1.textContent = '0';
-			document.querySelector('.player-0-panel').classList.toggle('active');
-			document.querySelector('.player-1-panel').classList.toggle('active');
-			dice.style.display = 'none';
+			nextPlayer();
 		}
-		document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+		document.querySelector(`#current-${activePlayer}`).textContent = '' + currentTotal;
 	}
 	
+	// add an event listener to the hold btn
+	document.querySelector('.btn-hold').addEventListener('click', hold);
+	function hold(e) {
+		// save current score to the player's global score
+		scores[activePlayer] += currentTotal;
+		
+		// display the player's global score
+		document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+		nextPlayer();
+		
+		// check if the player has won the game
+		
+	}
+
+	function nextPlayer() {
+		// reset the players current score and switch to the other player
+		currentTotal = 0;
+		activePlayer = (activePlayer === 0)? 1 : 0;
+		
+		// reset the current scores & update ui
+		current0.textContent = current1.textContent = '0';
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		dice.style.display = 'none';
+	}
+
+
 });
+
+
