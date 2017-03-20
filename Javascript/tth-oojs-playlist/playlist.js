3388fe68da4a9324926198edf8cae0aa2704a167
bill fero <billfero@gmail.com>
2017-03-20 19:10:37 +0000

    impl Playlist methods

--- Javascript/tth-oojs-playlist/playlist.js
+++ Javascript/tth-oojs-playlist/playlist.js
@@ -3,22 +3,35 @@ function Playlist() {
 	this.nowPlayingIndex = 0;
 }
 
+Playlist.prototype.add = function (song) {
+	this.songs.push(song);
+};
+
 Playlist.prototype.play = function () {
-	
+	let currentSong = this.songs[this.nowPlayingIndex];
+	currentSong.play();
 };
 
 Playlist.prototype.stop = function () {
-	
-};
-
-Playlist.prototype.add = function () {
-	
+	let currentSong = this.songs[this.nowPlayingIndex];
+	currentSong.stop();
 };
 
 Playlist.prototype.next = function () {
-	
+	this.stop(); // stop the current song
+	this.nowPlayingIndex++; // move to the next song in the array
+	if(this.nowPlayingIndex === this.songs.length){
+		// reached the end, play from the first song
+		this.nowPlayingIndex = 0;
+	}
+	this.play();
 };
 
-Playlist.prototype.renderElement = function () {
-	
+Playlist.prototype.renderElement = function (list) {
+	// clear any songs, iterate over the [playlist and display the songs
+	list.innerHTML = '';
+	this.songs.forEach(function (song, i) {
+		// build the song list, appending each song in turn
+		list.innerHTML += song.toHTML();
+	})
 };
