94150648971cf10fda130bae92dcd4405ce162e2
bill fero <billfero@gmail.com>
2017-03-20 18:38:06 +0000

    impl Song function constructor and method skeletons

---
+++ Javascript/tth-oojs-playlist/song.js
@@ -0,0 +1,18 @@
+function Song(title, artist, duration) {
+	this.title = title;
+	this.artist = artist;
+	this.duration = duration;
+	this.isPlaying = false;
+}
+
+Song.prototype.play = function () {
+	
+};
+
+Song.prototype.stop = function () {
+	
+};
+
+Song.prototype.toHTML = function () {
+	
+};
