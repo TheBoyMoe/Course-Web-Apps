30490087444a7868261b3599bcb64c6e863246ad
bill fero <billfero@gmail.com>
2017-03-20 17:09:21 +0000

    update app file

--- Javascript/tth-oojs-quiz/app.js
+++ Javascript/tth-oojs-quiz/app.js
@@ -1,1 +1,10 @@
-/* Starts the app & manages everything */
+/* Creates the quiz and displays the first question */
+
+let questions = [
+	new Question('Who was the first President of the United States?',
+				['George Washington', 'Thomas Jefferson'], 'George Washington'),
+	new Question('What is the answer to the Ultimate Question of Life, the Universe and Everything?',
+				['Pi', '42'], '42')
+];
+
+let quiz = new Quiz(questions);
