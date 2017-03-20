851f5cd5f74996718db6aa14b121009bfd89c902
bill fero <billfero@gmail.com>
2017-03-20 19:01:31 +0000

    define Playlist function constructor and method skeleton

---
+++ Javascript/tth-oojs-playlist/playlist.js
@@ -0,0 +1,24 @@
+function Playlist() {
+	this.songs  = [];
+	this.nowPlayingIndex = 0;
+}
+
+Playlist.prototype.play = function () {
+	
+};
+
+Playlist.prototype.stop = function () {
+	
+};
+
+Playlist.prototype.add = function () {
+	
+};
+
+Playlist.prototype.next = function () {
+	
+};
+
+Playlist.prototype.renderElement = function () {
+	
+};
