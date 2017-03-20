2ebe2d53f44264ab2f581cc640fb36af7f705457
bill fero <billfero@gmail.com>
2017-03-20 18:58:19 +0000

    impl Song obj

--- Javascript/tth-oojs-playlist/song.js
+++ Javascript/tth-oojs-playlist/song.js
@@ -6,13 +6,24 @@ function Song(title, artist, duration) {
 }
 
 Song.prototype.play = function () {
-	
+	this.isPlaying = true;
 };
 
 Song.prototype.stop = function () {
-	
+	this.isPlaying = false;
 };
 
 Song.prototype.toHTML = function () {
-	
+	let htmlString = '<li';
+	if(this.isPlaying){
+		htmlString += ' class="current"';
+	}
+	htmlString += '>';
+	htmlString += this.title;
+	htmlString += ' - ';
+	htmlString += this.artist;
+	htmlString += '<span class="duration">';
+	htmlString += this.duration;
+	htmlString += '</span></li>';
+	return htmlString;
 };
