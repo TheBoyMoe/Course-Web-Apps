a06f41a926c067d8fadd721c24dbaca3d56ca309
bill fero <billfero@gmail.com>
2017-03-20 15:28:38 +0000

    impl quiz object

--- Javascript/tth-oojs-quiz/quiz.js
+++ Javascript/tth-oojs-quiz/quiz.js
@@ -2,4 +2,30 @@
 	- collection of questions
 	- similar to playlist in media player app*/
 
-let questions = [];
+function Quiz() {
+	this.questions = [];
+	this.currentQuestion = 0;
+	this.currentScore = 0;
+}
+
+Quiz.prototype.next = function (container, progress, score) {
+	this.currentQuestion++;
+	if(this.currentQuestion === this.questions.length) {
+		this.currentQuestion = 0;
+		this.renderScore(progress, score);
+		
+	} else this.renderElement(container);
+};
+
+
+Quiz.prototype.renderElement = function (container) {
+	container.innerHTML = this.questions[this.currentQuestion].toHTML();
+};
+
+Quiz.prototype.renderProgress = function (progress) {
+	progress.innerHTML = `Question ${this.currentQuestion} out of ${this.questions.length}`;
+};
+
+Quiz.prototype.renderScore = function (progress, score) {
+	progress.innerHTML = `You answered ${score} correctly`;
+};
