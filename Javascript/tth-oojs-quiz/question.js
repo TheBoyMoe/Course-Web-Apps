c6a0884c1057d0406c07039966fcb9f84b713308
bill fero <billfero@gmail.com>
2017-03-20 15:41:05 +0000

    amend question obj prototype to check for correct answer and remove toHTML method

--- Javascript/tth-oojs-quiz/question.js
+++ Javascript/tth-oojs-quiz/question.js
@@ -1,22 +1,27 @@
-function Question(question, answers, correct) {
+function Question(question, choices, answer) {
 	this.question = question;
-	this.answers = answers;
-	this.correct = correct;
+	this.choices = answers;
+	this.answer = answer;
 }
 
-Question.prototype.toHTML = function () {
-	
-	let htmlString = '<h2 id="question" class="headline-secondary--grouped">';
-	htmlString += this.question;
-	htmlString += '</h2>';
-	htmlString += '<p id="choice0">';
-	htmlString += this.answers[0];
-	htmlString += '</p>';
-	htmlString += '<button id="guess0" class="btn--default">Select Answer</button>';
-	htmlString += '<p id="choice1">';
-	htmlString += this.answers[1];
-	htmlString += '</p>';
-	htmlString += '<button id="guess1" class="btn--default">Select Answer</button>';
-	
-	return htmlString;
+
+Question.prototype.isCorrectAnswer = function (choice) {
+	return this.answer === choice;
 };
+
+// Question.prototype.toHTML = function () {
+//
+// 	let htmlString = '<h2 id="question" class="headline-secondary--grouped">';
+// 	htmlString += this.question;
+// 	htmlString += '</h2>';
+// 	htmlString += '<p id="choice0">';
+// 	htmlString += this.answers[0];
+// 	htmlString += '</p>';
+// 	htmlString += '<button id="guess0" class="btn--default">Select Answer</button>';
+// 	htmlString += '<p id="choice1">';
+// 	htmlString += this.answers[1];
+// 	htmlString += '</p>';
+// 	htmlString += '<button id="guess1" class="btn--default">Select Answer</button>';
+//
+// 	return htmlString;
+// };
--- Javascript/tth-oojs-quiz/quiz_ui.js
+++ Javascript/tth-oojs-quiz/quiz_ui.js
@@ -1,2 +1,12 @@
 /* handles all the updates to the ui
  	- cache references to all the elements of importance */
+
+let container = document.getElementById('container');
+let progress = document.getElementById('progress');
+let guess0 = document.getElementById('guess0');
+let guess1 = document.getElementById('guess1');
+
+// impl btn onclick handlers
+guess0.onclick = function () {
+	
+}
