893364b6d95f5cee930fd395e3ac30a2a1474c9b
bill fero <billfero@gmail.com>
2017-03-26 23:40:55 +0100

    define app architecture, data, ui and controller modules - controller linking the data and ui modules together

--- Javascript/udemy/complete-js/budget-app/js/app.js
+++ Javascript/udemy/complete-js/budget-app/js/app.js
@@ -1,3 +1,38 @@
 /**
  * Created by theboymo on 15/03/17.
  */
+
+// MODEL
+let budgetController = (function () {
+	let x = 23;
+	
+	let add = function (a) {
+		return x + a;
+	};
+	
+	return {
+		api: function (b) {
+			return add(b);
+		}
+	}
+	
+})();
+
+// VIEW
+let uiController = (function () {
+	// more to come...
+	
+	
+})();
+
+// APP CONTROLLER -- connects the other two modules
+let appController = (function (budgetCtrl, uiCtrl) {
+	let a = budgetCtrl.api(5);
+	
+	return {
+		anotherApi: function () {
+			return a;
+		}
+	}
+	
+})(budgetController, uiController);
