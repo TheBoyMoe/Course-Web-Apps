9434527ce15fdf2d40c54bb0771c1aad4a1cd4ee
bill fero <billfero@gmail.com>
2017-03-27 23:19:32 +0100

    disable dummy code

--- Javascript/udemy/complete-js/budget-app/js/app.js
+++ Javascript/udemy/complete-js/budget-app/js/app.js
@@ -4,45 +4,52 @@
 
 // MODEL
 const budgetController = (() => {
-	let x = 23;
+	'use strict';
+	// let x = 23;
+	//
+	// let add = function (a) {
+	// 	return x + a;
+	// };
+	//
+	// return {
+	// 	api: function (b) {
+	// 		return add(b);
+	// 	}
+	// }
 	
-	let add = function (a) {
-		return x + a;
-	};
 	
-	return {
-		api: function (b) {
-			return add(b);
-		}
-	}
 	
 })();
 
 // VIEW
 const uiController = (() => {
 	'use strict';
-	return {
-		prop: 6,
-		api: () => {
-			return `Hello from the UI Controller`;
-		}
-	}
+	// return {
+	// 	prop: 6,
+	// 	api: () => {
+	// 		return `Hello from the UI Controller`;
+	// 	}
+	// }
+	
+	
 	
 })();
 
 // APP CONTROLLER -- connects the other two modules
 const appController = ((budgetCtrl, uiCtrl) => {
 	'use strict';
-	let a = budgetCtrl.api(5);
-	let b = uiCtrl.api();
-	let c = uiCtrl.prop;
-	return {
-		budgetApi: () => {
-			return a;
-		},
-		uiApi: () => {
-			return `${b} ${c}`;
-		}
-	}
+	// let a = budgetCtrl.api(5);
+	// let b = uiCtrl.api();
+	// let c = uiCtrl.prop;
+	// return {
+	// 	budgetApi: () => {
+	// 		return a;
+	// 	},
+	// 	uiApi: () => {
+	// 		return `${b} ${c}`;
+	// 	}
+	// }
+	
+	
 	
 })(budgetController, uiController);
