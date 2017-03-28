c7d5b78134831eb6ef7bb6d4c873707c8b945bef
bill fero <billfero@gmail.com>
2017-03-28 23:20:28 +0100

    impl income and expense function constructors

--- Javascript/udemy/complete-js/budget-app/js/app.js
+++ Javascript/udemy/complete-js/budget-app/js/app.js
@@ -17,7 +17,17 @@ const budgetController = (() => {
 	// 	}
 	// }
 	
+	const Expense = (id, description, value) => {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 	
+	const Income = (id, description, value) => {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 	
 })();
 
