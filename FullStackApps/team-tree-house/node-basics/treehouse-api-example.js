2aa19013a28beb3bdb5530d618aa966e95db4fec
bill fero <billfero@gmail.com>
2017-04-03 14:00:02 +0100

    add an error handler

--- FullStackApps/team-tree-house/node-basics/treehouse-api-example.js
+++ FullStackApps/team-tree-house/node-basics/treehouse-api-example.js
@@ -10,6 +10,8 @@
  	[2] https://nodejs.org/api/https.html#https_https_get_options_callback
  	[3] https://nodejs.org/api/http.html#http_http_status_codes
  	[4] https://nodejs.org/api/modules.html#modules_module_require_id
+ 	[5] https://nodejs.org/api/stream.html#stream_event_data
+ 	[6] https://nodejs.org/api/stream.html#stream_event_end
  	
 */
 
@@ -26,13 +28,34 @@ const http = require('http');
 const username = 'williamfero';
 
 // 1. connect to the treehouse api (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, (res)=>{
-	console.dir(res);
+const request = https.get(`https://teamtreehouse.com/${username}.json`, (response)=>{
+	console.log(response.statusCode);
+	let str = '';
+	// 2. read the json data - the res obj executes a 'data' event when the data is received
+	res.on('data', (data)=>{
+		// data obj - separate data packets in the form of a buffer read from the stream
+		// convert to a string using toString(), and concatenate together
+		// when ever you see a 'data' event in node, there's always an 'end' event
+		// which indicates when reading data from the stream has completed
+		
+		// console.log('data', data.toString());
+		
+		str += data.toString();
+	});
 	
-	// 2. read the json data
+	// display the json str when the end of the stream has been reached
+	response.on('end', ()=>{
+		console.log(str);
+	})
 	
 	// 3. parse the json string
 	
+});
+
+// error handler - if the server responds with an error, and an error handler
+// has not been set - error thrown
+request.on('error', (err)=>{
+	console.error('Server error', err.message);
 });
 
 
