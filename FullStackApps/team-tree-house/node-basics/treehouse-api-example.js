beb2126b18a5bc959d2ee6bd21cda8e4eb9577d6
bill fero <billfero@gmail.com>
2017-04-03 11:01:47 +0100

    outline the problem - connect to treehouse api, fetch and parse json string, display the result

---
+++ FullStackApps/team-tree-house/node-basics/treehouse-api-example.js
@@ -0,0 +1,10 @@
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+
+// example student profile url
+// https://teamtreehouse.com/williamfero.json
+
+// 1. connect to the treehouse api (https://teamtreehouse.com/username.json)
+// 2. read the json data
+// 3. parse the json string
+// 4. display the data
