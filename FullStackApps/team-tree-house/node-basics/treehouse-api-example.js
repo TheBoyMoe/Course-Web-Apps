42efaa5ac4a73f11b50491cb4f030b84284a2692
bill fero <billfero@gmail.com>
2017-04-03 14:34:44 +0100

    refactor http request into a function

--- FullStackApps/team-tree-house/node-basics/treehouse-api-example.js
+++ FullStackApps/team-tree-house/node-basics/treehouse-api-example.js
@@ -13,6 +13,7 @@
  	[5] https://nodejs.org/api/stream.html#stream_event_data
  	[6] https://nodejs.org/api/stream.html#stream_event_end
  	[7] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
+ 	[8] https://nodejs.org/docs/latest/api/process.html#process_process_argv
  	
 */
 
@@ -25,48 +26,52 @@
 'use strict';
 
 const https = require('https');
-const http = require('http');
-const username = 'williamfero';
 
-// 1. connect to the treehouse api (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, (response)=>{
-	console.log(response.statusCode);
-	
-	let str = '';
-	// 2. read the json data - the res obj executes a 'data' event when the data is received
-	response.on('data', (data)=>{
-		// data obj - separate data packets in the form of a buffer read from the stream
-		// convert to a string using toString(), and concatenate together
-		// when ever you see a 'data' event in node, there's always an 'end' event
-		// which indicates when reading data from the stream has completed
+const getUserProfile = (username)=>{
+	// 1. connect to the treehouse api (https://teamtreehouse.com/username.json)
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, (response)=>{
+		// console.log(response.statusCode);
 		
-		// console.log('data', data.toString());
+		let str = '';
+		// 2. read the json data - the res obj executes a 'data' event when the data is received
+		response.on('data', (data)=>{
+			// data obj - separate data packets in the form of a buffer read from the stream
+			// convert to a string using toString(), and concatenate together
+			// when ever you see a 'data' event in node, there's always an 'end' event
+			// which indicates when reading data from the stream has completed
+			
+			// console.log('data', data.toString());
+			
+			str += data.toString();
+		});
 		
-		str += data.toString();
+		// display the json str when the end of the stream has been reached
+		response.on('end', ()=>{
+			// console.log(str);
+			
+			// 3. parse the json string (using native js obj) into an object so that we can
+			// retrieve the necessary properties programmatically
+			const userProfile = JSON.parse(str);
+			//console.dir(userProfile);
+			let message = printMessage(username, userProfile.badges.length, userProfile.points.JavaScript);
+			console.log(message);
+		})
+		
+	});
+
+	// error handler - if the server responds with an error, and an error handler
+	// has not been set - error thrown
+	request.on('error', (err)=>{
+		console.error('Server error', err.message);
 	});
 	
-	// display the json str when the end of the stream has been reached
-	response.on('end', ()=>{
-		// console.log(str);
-		
-		// 3. parse the json string (using native js obj) into an object so that we can
-		// retrieve the necessary properties programmatically
-		const userProfile = JSON.parse(str);
-		//console.dir(userProfile);
-		let message = printMessage(username, userProfile.badges.length, userProfile.points.JavaScript);
-		console.log(message);
-	})
-	
-});
-
-// error handler - if the server responds with an error, and an error handler
-// has not been set - error thrown
-request.on('error', (err)=>{
-	console.error('Server error', err.message);
-});
-
+};
 
 // 4. display the data
 const printMessage = (username, badgeCount, points)=>{
 	return `${username} has ${badgeCount} badges and ${points} points in javascript!`;
 };
+
+getUserProfile('chalkers');
+getUserProfile('williamfero');
+getUserProfile('alenaholligan');
