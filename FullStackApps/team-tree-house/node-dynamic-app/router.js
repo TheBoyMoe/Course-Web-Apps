9c97d76c21f48b1b50fee261963f220c69402935
bill fero <billfero@gmail.com>
2017-04-04 15:34:07 +0100

    fetch user profile info from treehouse, display error if user not found

--- FullStackApps/team-tree-house/node-dynamic-app/router.js
+++ FullStackApps/team-tree-house/node-dynamic-app/router.js
@@ -1,3 +1,6 @@
+'use strict';
+const Profile = require('./profile.js');
+
 // handle home routes, ie, GET/POST '/'
 const homeRoute = (req, res)=>{
 	if(req.url === '/') {
@@ -14,8 +17,27 @@ const userRoute = (req, res)=>{
 	if(username.length > 0){
 		res.writeHead(200, {'content-type': 'text/plain'});
 		res.write('Header\n');
-		res.write(`${username}\n`);
-		res.end('Footer\n');
+		
+		// fetch the users profile from treehouse
+		let profile = new Profile(username);
+		profile.on('end', (profileJson)=>{
+			
+			// store user info
+			let info = {
+				avatarUrl: profileJson.gravatar_url,
+				username: profileJson.profile_name,
+				badges: profileJson.badges.length,
+				jsPoints: profileJson.points.JavaScript
+			};
+			
+			res.write(`${info.username} has ${info.badges} badges and ${info.jsPoints} javascript points\n`);
+			res.end('Footer\n');
+		});
+		profile.on('error', (err)=>{
+			res.write(err.message + '\n');
+			res.end('Footer\n');
+		});
+		
 	}
 };
 
