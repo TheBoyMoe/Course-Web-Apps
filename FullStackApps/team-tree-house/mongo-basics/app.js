73c5bf0f628558dcc1992752f6171bf7350e946f
bill fero <billfero@gmail.com>
2017-04-06 14:17:55 +0100

    more basic commands

--- FullStackApps/team-tree-house/mongo-basics/app.js
+++ FullStackApps/team-tree-house/mongo-basics/app.js
@@ -9,6 +9,7 @@
 	[6] https://www.mongodb.com/nosql-explained
 	[7] https://docs.mongodb.com/manual/reference/sql-comparison/
 	[8] https://docs.mongodb.com/master/reference/method/load/
+	[9] https://docs.mongodb.com/master/reference/method/Date/ (date formats)
 	
 	
 	Mongo basics:
@@ -37,27 +38,48 @@
 		> db.post.insert({.....})
 	
 	6. to view db collections in the current database
-		> show collections
+		> show collections OR > db.getCollectionNames()
+	   
 	   to show databases
 	    > show dbs
 	
 	7. to list all records in that collection, i.e posts in this example
 		> db.posts.find() // returns a collection of all the records
+	   
 	   to limit the number of documents returned
 	   	> db.posts.find().limit(2) // returns 2 docs
+	   
 	   to view individual docs within a collection query it like an array
 	   	> db.posts.find()[0]
+	   
 	   to display the number of documents in a collection
 	   	> db.posts.count()
+	   
+	   to display the indexes (created automatically to facilitate quick lookups) on a particular collection
+	   (view which collection property is defined as the index, generally the _id key)
+	   	> db.posts.getIndexes()
+	   
+	   can be beneficial to create your own indexes - post title - aid searching for posts based on title
+	   to create your own index on a particular document property use the createIndex() method
+	    => pass in an object with the property to be indexed set as the key, and either a value of 1 (ASC) or -1 (DESC)
+	     	and an optional object with a set of options as a 2nd parameter
+	    > db.posts.createIndex({title: 1})
+	    
+	    run db.posts.getIndexes() to retrieve info about the collections indexes
+	    
+	    you can delete an index using dropIndex() method
+	     - use getIndexes() to get the index name, not necessarily the prop name used in it's creation
+	    > db.posts.dropIndex('title_1')
 	
 	8. to view all the files in the current dir (relative to the folder you were in when you launched the shell)
 	 	> ls()
 	 	
-	9. to load a file
+	9. to load a file (the load command is relative to the cwd (current working directory) of where you launch the mongo shell from.)
 	 	> load('path/to/file')
 	 	
 	10. you can assign a document to a variable (doesn't support es6 let)
 	 	> var post = db.posts.find()[1]
+	 	
 	 	you can reference properties of that document using dot notation.
 	 	fields within a document can reference fields in other collections,
 	 	e.g posts.author references the author in the users collection
