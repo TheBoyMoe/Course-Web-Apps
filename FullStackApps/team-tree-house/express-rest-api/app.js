6d5c3be3498b0e7e282ea6da9c3199f3645be5e5
bill fero <billfero@gmail.com>
2017-04-06 20:41:27 +0100

    add answer routes

--- FullStackApps/team-tree-house/express-rest-api/app.js
+++ FullStackApps/team-tree-house/express-rest-api/app.js
@@ -7,6 +7,7 @@
 	[4] https://developers.google.com/google-apps/calendar/v3/reference/events#resource-representations (example of resource representation)
 	[5] https://www.getpostman.com/docs/introduction
 	[6] http://expressjs.com/en/4x/api.html#router
+	[7] https://www.npmjs.com/package/morgan
 	
 	
  */
@@ -15,6 +16,7 @@
 const express = require('express');
 const jsonParser = require('body-parser').json;
 const routes = require('./routes.js');
+const logger = require('morgan');
 const app = express();
 
 /*
@@ -22,7 +24,12 @@ const app = express();
 	 - : tells express what follows is a parameter
 	 - use the query property on the req obj to access query parameters (req.params obj)
 	 - you can pass data from one middleware function to another by assigning it as a property to the req obj
+	 - use body-parser module to parse req body into json
+	 - use Morgan logger to log http status codes as well as our routes
 */
+
+// configures express to golor code the http status codes
+app.use(logger('dev'));
 
 // parse the requests body as json and make it accessible from the req obj's body property
 app.use(jsonParser());
--- FullStackApps/team-tree-house/express-rest-api/routes.js
+++ FullStackApps/team-tree-house/express-rest-api/routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-// GET '/questions' route - the 'questions' parameter has been stripped away by the app since it's already matched
+// GET '/questions' route - the 'questions' parameter has been stripped away by the app since it's already matched - app.use('/questions', routes )
 // the callback (route handler) is invoked when the router receives a request that matches the route and verb
 router.get('/', (req, res)=>{
 	// return all the questions - requires a db connection
@@ -19,12 +19,50 @@ router.post('/', (req, res)=>{
 });
 
 
-// GET 'questions/:id' - return a specific question
-router.get('/:id', (req, res)=>{
+// GET '/questions/:id' - return a specific question
+router.get('/:qID', (req, res)=>{
 	res.json({
-		response: `You requested the question with ID: ${req.params.id}`
+		response: `You requested the question with ID: ${req.params.qID}`
 	})
 });
 
+// POST /questions/:id/answers - route for creating answers to a specific question
+router.post('/:qID/answers', (req, res)=>{
+	res.json({
+		response: `You tried to POST a request to /answers`,
+		questionId: req.params.qID,
+		body: req.body
+	})
+});
+
+// PUT '/questions/:qID/answers/:aID' - route for updating/editing a specific answer
+router.put('/:qID/answers/:aID', (req, res)=>{
+	res.json({
+		response: 'You sent a PUT request to /answers',
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body
+	})
+});
+
+// DELETE '/questions/:qID/answers/:aID' - route for deleting specific answers to questions
+router.delete('/:qID/answers/:aID', (req, res)=>{
+	res.json({
+		response: 'You sent a DELETE request to /answers',
+		questionId: req.params.qID,
+		answerId: req.params.aID
+	})
+});
+
+// POST '/questions/:qID/answers/:aID/vote-up'
+// POST '/questions/:qID/answers/:aID/vote-down'
+router.post('/:qID/answers/:aID/vote-:dir', (req, res)=>{
+	res.json({
+		response: `You sent a POST request to /vote-${req.params.dir}`,
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		vote: req.params.dir
+	})
+});
 
 module.exports = router;
