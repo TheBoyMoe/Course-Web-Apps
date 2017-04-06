733d33ecd2889e4aad8f21236662c986b1542b86
bill fero <billfero@gmail.com>
2017-04-06 12:54:05 +0100

    examples of simple mongo db commands

--- FullStackApps/team-tree-house/mongo-basics/app.js
+++ FullStackApps/team-tree-house/mongo-basics/app.js
@@ -19,7 +19,7 @@
 		$ mongo
 		
 	3. to quit the shell and Ctrl + C to stop mongod
-		$ quit()
+		> quit()
 	
 	note: you can start mongod as a bkgd service and carry on using the same cmd tab
 		$ sudo service mongod start
@@ -31,17 +31,35 @@
 		$ ps aux OR ps -ef => look for mongod entry (use 'kill -9 [pid]' to kill the service)
 		
 	4. to create and switch to a database
-		$ use [database_name]
+		> use [database_name]
 		
 	5. to insert documents - post creates a collection, insert adds json obj
-		$ db.post.insert({.....})
+		> db.post.insert({.....})
 	
-	6. to view db collections
-		$ show collections
+	6. to view db collections in the current database
+		> show collections
+	   to show databases
+	    > show dbs
 	
-	7. to list all records
-		$ db.post.find()
+	7. to list all records in that collection, i.e posts in this example
+		> db.posts.find() // returns a collection of all the records
+	   to limit the number of documents returned
+	   	> db.posts.find().limit(2) // returns 2 docs
+	   to view individual docs within a collection query it like an array
+	   	> db.posts.find()[0]
+	   to display the number of documents in a collection
+	   	> db.posts.count()
 	
-	 
+	8. to view all the files in the current dir (relative to the folder you were in when you launched the shell)
+	 	> ls()
+	 	
+	9. to load a file
+	 	> load('path/to/file')
+	 	
+	10. you can assign a document to a variable (doesn't support es6 let)
+	 	> var post = db.posts.find()[1]
+	 	you can reference properties of that document using dot notation.
+	 	fields within a document can reference fields in other collections,
+	 	e.g posts.author references the author in the users collection
 
  */
