f9636096bb8141345b8c096e7598dd6c31ef7250
bill fero <billfero@gmail.com>
2017-04-09 18:51:17 +0100

    inheritance using Object create method

--- FullStackApps/udemy/understand-nodejs/modules-in-js/function-constructors.js
+++ FullStackApps/udemy/understand-nodejs/modules-in-js/function-constructors.js
@@ -6,13 +6,38 @@ function Person(firstname, lastname, age) {
 	this.age = age;
 }
 
-const person = new Person('Tom', 'Jones', 66);
+const tom = new Person('Tom', 'Jones', 66);
 const peter = new Person('Peter', 'Jones', 54);
-console.log(`${person.firstname} ${person.lastname}, age ${person.age}`);
+console.log(`${tom.firstname} ${tom.lastname}, age ${tom.age}`);
 
 Person.prototype.greet = function () {
 	console.log(`Hello my name is ${this.firstname} ${this.lastname} and I am ${this.age} years old`);
 };
 
-person.greet();
+tom.greet();
 peter.greet();
+
+
+// another pattern in js for creating objects and using prototypal inheritance
+
+const person = {
+	firstname: '',
+	lastname: '',
+	greet() {
+		console.log(`${this.firstname} ${this.lastname}`);
+	}
+};
+
+// create your obj instances using Object.create() - in this pattern the person obj is used as the prototype
+// for each obj instance - you inherit the props and methods
+//  - when you define props with the same name on the instance, you hide the ones inherited
+let john = Object.create(person);
+john.firstname = 'John'; // overrides the prop in the prototype
+john.lastname = 'Smith';
+john.greet();
+
+let jane = Object.create(person);
+jane.firstname = 'Jane';
+jane.lastname = 'Smith';
+jane.greet();
+
