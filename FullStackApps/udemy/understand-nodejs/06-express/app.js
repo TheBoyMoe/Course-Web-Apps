e3f35e7737e9860e658d624eec7a78cacf5ed6b3
bill fero <billfero@gmail.com>
2017-04-10 18:05:25 +0100

    using static files

--- FullStackApps/udemy/understand-nodejs/06-express/app.js
+++ FullStackApps/udemy/understand-nodejs/06-express/app.js
@@ -14,8 +14,19 @@ const data = [
 	}
 ];
 
+// define location of static files - redirect any requests for files in
+// the assets folder to public
+app.use('/assets', express.static(`${__dirname}/public`));
+
 app.get('/', (req, res)=>{
-	res.send(`<html><head></head><body><h1>Express server if listening on port ${port}...</h1></body></html>`);
+	res.send(`<html>
+					<head>
+						<link href="assets/styles.css" rel="stylesheet" type="text/css"/>
+					</head>
+					<body>
+						<h1>Express server if listening on port ${port}...</h1>
+					</body>
+				</html>`);
 });
 
 // the colon, ':', indicates that the value that follows can be anything, is dynamic
