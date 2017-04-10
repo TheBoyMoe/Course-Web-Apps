4f09aaa41a442674ae98c24cb3f964ef8fd11458
bill fero <billfero@gmail.com>
2017-04-10 22:33:22 +0100

    launch app with command

--- FullStackApps/udemy/complete-node/01-note-app/app.js
+++ FullStackApps/udemy/complete-node/01-note-app/app.js
@@ -3,3 +3,25 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+let command = process.argv[2]; // return the 3rd element
+let message;
+
+switch (command){
+	case 'add':
+		message = 'Adding a new note';
+		break;
+	case 'list':
+		message = 'Listing all notes';
+		break;
+	case 'read':
+		message = 'Reading note';
+		break;
+	case 'remove':
+		message = 'Remove note';
+		break;
+	default:
+		message = 'Command not recognised'
+}
+
+console.log('Command: ', message);
