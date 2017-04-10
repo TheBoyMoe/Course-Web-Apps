0373dd95482ab034586a0a27f9de6f4bdda3f0cc
bill fero <billfero@gmail.com>
2017-04-10 13:29:59 +0100

    respond with a html file

--- FullStackApps/udemy/understand-nodejs/05-web-server/app.js
+++ FullStackApps/udemy/understand-nodejs/05-web-server/app.js
@@ -1,11 +1,15 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 
 // http inherits from the event emitter - the callback is invoked when
 // the 'request' event is triggered
 http.createServer((req, res)=>{
 	
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello. My name Node, your friendly neighborhood web server......\n');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	// res.end('Hello. My name Node, your friendly neighborhood web server......\n');
+	const html = fs.readFileSync(`${__dirname}/index.html`);
+	res.end(html);
 	
-}).listen(8080, '127.0.0.1');
+	
+}).listen(3000, '127.0.0.1');
