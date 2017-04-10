c4bbbbfb08ef4551d5fd6688e0e562470c7c8493
bill fero <billfero@gmail.com>
2017-04-10 14:32:43 +0100

    simple example of a template

--- FullStackApps/udemy/understand-nodejs/05-web-server/app.js
+++ FullStackApps/udemy/understand-nodejs/05-web-server/app.js
@@ -8,8 +8,11 @@ http.createServer((req, res)=>{
 	
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	// res.end('Hello. My name Node, your friendly neighborhood web server......\n');
-	const html = fs.readFileSync(`${__dirname}/index.html`);
+	// returns a buffer since char encoding has not been specified
+	// const html = fs.readFileSync(`${__dirname}/index.html`);
+	let html = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
+	const message = 'Hello, my name is Michael Caine';
+	html = html.replace('{message}', message);
 	res.end(html);
-	
 	
 }).listen(3000, '127.0.0.1');
