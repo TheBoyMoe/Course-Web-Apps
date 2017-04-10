fd67b846a112c815a47f5e10aab99d8648bdbec4
bill fero <billfero@gmail.com>
2017-04-10 22:15:28 +0100

    inintial commit - node notes app

---
+++ FullStackApps/udemy/complete-node/01-note-app/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes');
---
+++ FullStackApps/udemy/complete-node/01-note-app/notes.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports.addNote = ()=>{
+	console.log('add new note');
+	return 'New note';
+};
