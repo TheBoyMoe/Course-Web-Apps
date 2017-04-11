ebe17944adaa0a1926105de8b9e5623c41c0b0b5
bill fero <billfero@gmail.com>
2017-04-11 19:41:28 +0100

    impl cli to take dynamic addresses and return address and lat and lng coords

--- FullStackApps/udemy/complete-node/02-weather-app/app.js
+++ FullStackApps/udemy/complete-node/02-weather-app/app.js
@@ -1,8 +1,29 @@
 'use strict';
 const request = require('request');
+const yargs = require('yargs');
+
+// config cmd line entry using yargs
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Fetch the weather for the entered address',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+// console.log(argv);
+
+// encodeURIComponent(str) / decodeURIComponent(encodedStr)
+// fetch the address from the cli and encode it
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20lomdard%20street%20london',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body)=>{
 	// print all objects
