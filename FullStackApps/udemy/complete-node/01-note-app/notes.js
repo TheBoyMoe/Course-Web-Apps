898b31acf06742182931eb6344b9942c1bd872e6
bill fero <billfero@gmail.com>
2017-04-11 10:42:30 +0100

    save new notes to the notes array and write to file

--- FullStackApps/udemy/complete-node/01-note-app/notes.js
+++ FullStackApps/udemy/complete-node/01-note-app/notes.js
@@ -1,7 +1,24 @@
 "use strict";
+const fs = require('fs');
+
 
 const addNote = (title, body)=>{
-	console.log('Add note: ', title, body);
+	let notes = [];
+	let note = {
+		title: title,
+		body: body
+	};
+	
+	// check if any notes have been previously saved
+	try {
+		let notesStr = fs.readFileSync(`${__dirname}/notes-data.json`, 'utf-8');
+		notes = JSON.parse(notesStr); // parse the string into js objs
+	} catch (e){
+		console.error('Error parsing saved notes', e.message);
+	}
+	
+	notes.push(note);
+	fs.writeFileSync(`${__dirname}/notes-data.json`, JSON.stringify(notes));
 };
 
 const getAll = ()=>{
