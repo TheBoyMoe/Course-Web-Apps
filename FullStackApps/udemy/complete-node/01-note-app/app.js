f6e02e1620d0f9386fbee37c75818f578da1f413
bill fero <billfero@gmail.com>
2017-04-11 09:35:57 +0100

    impl yargs package to read commands and notes from the cli

--- FullStackApps/udemy/complete-node/01-note-app/app.js
+++ FullStackApps/udemy/complete-node/01-note-app/app.js
@@ -1,27 +1,33 @@
 "use strict";
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
+const argv = yargs.argv;
 
-let command = process.argv[2]; // return the 3rd element
-let message;
+// return the 3rd element of argv [] on node process
+// let command = process.argv[2];
+let command = argv._[0]; // 1st item in _ [] on yargs obj
 
+// add props to the argv obj with --[prop_name], e.g node app.js --title '...' --body '.....'
+// any commands added following the file name are added to the _ array, e.g node app.js list file
 switch (command){
 	case 'add':
-		message = 'Adding a new note';
+		notes.add(argv.title, argv.body);
 		break;
 	case 'list':
-		message = 'Listing all notes';
+		notes.getAll();
 		break;
 	case 'read':
-		message = 'Reading note';
+		notes.getNote(argv.title);
 		break;
 	case 'remove':
-		message = 'Remove note';
+		notes.delete(argv.title);
 		break;
 	default:
-		message = 'Command not recognised'
+		let message = (command)? `${command} command not recognised`: 'Command not recognised';
+		console.log(message);
 }
 
-console.log('Command: ', message);
+console.log('Yargs: ', argv);
--- FullStackApps/udemy/complete-node/01-note-app/notes.js
+++ FullStackApps/udemy/complete-node/01-note-app/notes.js
@@ -1,6 +1,24 @@
 "use strict";
 
-module.exports.addNote = ()=>{
-	console.log('add new note');
-	return 'New note';
+const addNote = (title, body)=>{
+	console.log('Add note: ', title, body);
+};
+
+const getAll = ()=>{
+	console.log('Get all notes')
+};
+
+const getNote = (title)=>{
+	console.log('Get note: ', title);
+};
+
+const deleteNote = (title)=>{
+	console.log('Delete note: ', title);
+};
+
+module.exports = {
+	add: addNote,
+	getAll: getAll,
+	getNote: getNote,
+	delete: deleteNote
 };
