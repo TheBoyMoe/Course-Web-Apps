af0fc71ccc19e544508aa3ac42a6400dd1176df8
bill fero <billfero@gmail.com>
2017-04-11 22:06:21 +0100

    refactor fetch location data to be asynchronous using a callback

--- FullStackApps/udemy/complete-node/02-weather-app/app.js
+++ FullStackApps/udemy/complete-node/02-weather-app/app.js
@@ -1,6 +1,7 @@
 'use strict';
-const request = require('request');
+// const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 // config cmd line entry using yargs
 const argv = yargs
@@ -16,24 +17,28 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-// console.log(argv);
 
-// encodeURIComponent(str) / decodeURIComponent(encodedStr)
-// fetch the address from the cli and encode it
-let encodedAddress = encodeURIComponent(argv.address);
-
-request({
-	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json: true
-}, (error, response, body)=>{
-	// print all objects
-	// console.log(JSON.stringify(body, undefined, 2)); // number is indentation
-	if(error) console.log('Unable to connect to the Google Servers');
-	else if(body.status === 'ZERO_RESULTS') console.log('Unable to find the submitted address');
-	else if(body.status === 'OK') {
-		let item = body.results[0];
-		let itemLocation = item.geometry.location;
-		console.log(`Address: ${item['formatted_address']}, lat: ${itemLocation.lat}, lng: ${itemLocation.lng}`);
-	} else
-		console.log('Unknown error occurred, try again');
+geocode.fetchData(argv.address, (errorMessage, results)=>{
+	(errorMessage)? console.log(errorMessage): console.log(JSON.stringify(results, undefined, 2));
 });
+
+
+
+// // encodeURIComponent(str) / decodeURIComponent(encodedStr)
+// // fetch the address from the cli and encode it
+// let encodedAddress = encodeURIComponent(argv.address);
+
+// request({
+// 	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+// 	json: true
+// }, (error, response, body)=>{
+// 	// console.log(JSON.stringify(body, undefined, 2)); // number is indentation
+// 	if(error) console.log('Unable to connect to the Google Servers');
+// 	else if(body.status === 'ZERO_RESULTS') console.log('Unable to find the submitted address');
+// 	else if(body.status === 'OK') {
+// 		let item = body.results[0];
+// 		let itemLocation = item.geometry.location;
+// 		console.log(`Address: ${item['formatted_address']}, lat: ${itemLocation.lat}, lng: ${itemLocation.lng}`);
+// 	} else
+// 		console.log('Unknown error occurred, try again');
+// });
---
+++ FullStackApps/udemy/complete-node/02-weather-app/geocode/geocode.js
@@ -0,0 +1,38 @@
+'use strict';
+const request = require('request');
+
+const encodeAddress = (address)=>{
+	// encodeURIComponent(str) / decodeURIComponent(encodedStr)
+	// fetch the address from the cli and encode it
+	return encodeURIComponent(address);
+};
+
+const fetchData = (address, callback)=>{
+	let encodedAddress = encodeAddress(address);
+	request({
+		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+		json: true
+	}, (error, response, body)=>{
+		
+		if(error)
+			callback('Unable to connect to the Google Servers');
+		else if(body.status === 'ZERO_RESULTS')
+			callback('Unable to find the submitted address');
+		else if(body.status === 'OK') {
+			let item = body.results[0];
+			let itemLocation = item.geometry.location;
+			callback(undefined, {
+				Address: `${item['formatted_address']}`,
+				lat: `${itemLocation.lat}`,
+				lng: `${itemLocation.lng}`
+			}
+		);
+		} else
+			callback('Unknown error occurred, try again');
+	});
+	
+};
+
+module.exports = {
+	fetchData
+};
