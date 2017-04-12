d4bb43729739b6772b9d4f5a0f9f0bdee986ebf8
bill fero <billfero@gmail.com>
2017-04-12 22:58:49 +0100

    automate your tests with npm and nodemon

--- FullStackApps/udemy/complete-node/05-node-tests/utils/utils.test.js
+++ FullStackApps/udemy/complete-node/05-node-tests/utils/utils.test.js
@@ -1,6 +1,17 @@
 /*
 	References:
 	[1] http://mochajs.org/
+	
+	
+	Note:
+	- to automatically run all your tests when ever a file changes
+		$ nodemon --exec 'npm test'
+	
+	- to automate testing with npm, add the following line to the scripts section of package.json
+ 		"test-watch": "nodemon --exec \"npm test\""
+ 	- to run the tests, from the cli enter
+ 		$ npm run test-watch
+	
  */
 "use strict";
 const utils = require('./utils');
