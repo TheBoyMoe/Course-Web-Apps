6d6d8610f919b040b17336ce1ac869ed7536f99a
bill fero <billfero@gmail.com>
2017-04-12 15:19:35 +0100

    refactor weather app using axios and promises

--- FullStackApps/udemy/complete-node/03-weather-app-using-promises/app.js
+++ FullStackApps/udemy/complete-node/03-weather-app-using-promises/app.js
@@ -1,7 +1,12 @@
+/*
+	References:
+	[1] https://www.npmjs.com/package/axios
+	
+ */
+
 'use strict';
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
 
 // config cmd line entry using yargs
 const argv = yargs
@@ -17,21 +22,35 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodedUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-geocode.getLocation(argv.address, (errorMessage, results)=>{
-	if(errorMessage) {
-		console.log(errorMessage)
-	} else {
-		// fetch the weather with the retrieved lat lng results
-		weather.getWeather(results.lat, results.lng, (errorMessage, weather)=>{
-			if(errorMessage) {
-				console.log(errorMessage)
-			} else {
-				results.temperature = weather.temperature;
-				results.humidity = weather.humidity;
-				results.pressure = weather.pressure;
-				console.log(JSON.stringify(results, undefined, 2));
-			}
-		});
-	}
+axios.get(geocodedUrl)
+	.then((response)=>{
+		if(response.data.status === 'ZERO_RESULTS')
+			throw new Error('Unable to find that address'); // caught by the catch block
+		
+		const apikey = '840178050c6f75f3641a44e2f2f547e2';
+		const location = response.data.results[0].geometry.location;
+		const lat = location.lat, lng = location.lng;
+		const weatherUrl = `https://api.darksky.net/forecast/${apikey}/${lat},${lng}`;
+		
+		console.log(response.data.results[0].formatted_address);
+		
+		// chain a 2nd promise
+		return axios.get(weatherUrl);
+		
+	})
+	.then((response)=>{
+		const weatherInfo = response.data.currently;
+		const temperature = weatherInfo.temperature;
+		const apparentTemp = weatherInfo.apparentTemperature;
+		console.log(`It's currently ${temperature}, it feels like ${apparentTemp}.`);
+	})
+	.catch((error)=>{
+		//console.error(error);
+		if(error.code === 'ENOTFOUND')
+			console.error('Unable to fetch results.');
+		else
+			console.error(error.message);
 });
