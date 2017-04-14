2ab5a09b929c93b46ab14cf959a9a802c93d74e4
bill fero <billfero@gmail.com>
2017-04-14 17:26:31 +0100

    include validation in the data model

--- FullStackApps/udemy/complete-node/06-todo-app/server/server.js
+++ FullStackApps/udemy/complete-node/06-todo-app/server/server.js
@@ -2,6 +2,7 @@
 /*
 	References:
 	[1] http://mongoosejs.com/docs/guide.html
+	[2] http://mongoosejs.com/docs/validation.html (define validation on your schema/data model)
 	
  	 - mongoose doesn't support promises out of the box, uses callbacks by default
  	 	- configure it to use global.Promise - built in Promise library
@@ -10,6 +11,7 @@
  	 	- Mongodb has no data structure other than db consists of collections, collections contain documents
  	 	- Create the model using .model() - give it a name and pass in a settings obj which defines the docs property names and data types
  	 	- Mongoose automatically creates the database and collection (lowercase, and plural of model name) when inserting 1st doc
+ 	 	- mongoose will coerce numbers/booleans into strings when passing bools/nums when a string is expected
  	 	
   */
 'use strict';
@@ -18,25 +20,28 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// data model - props optional, since they've not been defined as req'd
+// data model - props not been defined as req'd are optional
 const Todo = mongoose.model('Todo', {
 	text: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 6,
+		trim: true // remove leading/trailing spaces
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false // don't set re'd on props with default
 	},
 	completedAt: {
-		type: Number
+		type: Number,
+		default: null
 	}
 });
 
 // to create a new object, call Todo as if it were a constructor function
 // passing an object in which you specify property values, eg
 let newTodo = new Todo({
-	text: 'Going to the park with the dog',
-	completed: false,
-	completedAt: -1
+	text: 'Going to the park to annoy the dog'
 });
 
 // to save the obj, call save() - returns a promise - returns actual doc or error
