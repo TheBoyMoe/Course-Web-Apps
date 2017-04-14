b56a581abd21364d74ace48863e5fa46c49a92e4
bill fero <billfero@gmail.com>
2017-04-14 09:39:25 +0100

    create and populate a Users collection

--- FullStackApps/udemy/complete-node/06-todo-app/playground/mongodb-connect.js
+++ FullStackApps/udemy/complete-node/06-todo-app/playground/mongodb-connect.js
@@ -7,23 +7,44 @@
 	[5] http://mongodb.github.io/node-mongodb-native/2.2/
 	[6] http://mongodb.github.io/node-mongodb-native/2.2/api/
 
+	Note:
+	- to launch Robomongo gui
+		$ /usr/local/bin/robomongo
  */
 "use strict";
 const MongoClient = require('mongodb').MongoClient;
 
+// use the url to the dbase
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
 	if(err) return console.error('Unable to connect to Mongodb server');
+	console.log('Successfully connected to Mongodb server');
 	
 	// Todo's collection - insert a document into the collection
-	db.collection('Todos').insertOne({
-		text: 'First todo added',
-		completed: false
+	// db.collection('Todos').insertOne({ // inserting the first record creates the db
+	// 	text: 'First todo added',
+	// 	completed: false
+	// }, (err, result)=>{
+	// 	if(err) return console.error('Unable to insert the record', err);
+	//
+	// 	// ops contains array of one or more records inserted
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+	
+	// User's collection - insert  a new user into the collection (name, age, location)
+	// each object is given a unique 12byte id => _id
+	//  - made up of a time stamp (no need for a date created field),
+	//  - unique machine identifier
+	//  - process id & counter
+	db.collection('Users').insertOne({
+		name: 'Grace Jones',
+		age: 58,
+		location: 'Paris, Tx'
 	}, (err, result)=>{
-		if(err) return console.error('Unable to insert the record', err);
+		if(err) return console.error('Unable to insert record');
 		
-		console.log(JSON.stringify(result.ops, undefined, 2)); // ops contains the records inserted
+		console.log('Insert successful: ', JSON.stringify(result.ops, undefined, 2));
 	});
 	
-	console.log('Successfully connected to Mongodb server');
+	
 	db.close();
 });
