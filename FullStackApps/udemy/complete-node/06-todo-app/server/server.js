9141048e3eb2d4d21d4f50765419be76f1b0a1e5
bill fero <billfero@gmail.com>
2017-04-15 10:10:03 +0100

    impl GET route to fetch individual todo items given an id

--- FullStackApps/udemy/complete-node/06-todo-app/server/server.js
+++ FullStackApps/udemy/complete-node/06-todo-app/server/server.js
@@ -6,6 +6,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -39,6 +40,37 @@ app.get('/todos', (req, res)=>{
 		res.status(400).send(err);
 	});
 });
+
+// get todo - return individual todo item
+app.get('/todos/:id', (req, res)=>{
+	let id = req.params.id; // grab the id supplied by the client
+	
+	// 1. check if id is valid
+	if(!ObjectID.isValid(id)){
+		res.status(404).send({
+			code: 404,
+			error: 'Invalid id'
+		});
+	} else {
+		// 2. query Todo collection - does it exist
+		Todo.findById(id).then((todo)=>{
+			if(!todo) return res.status(404).send({
+				code: 404,
+				error: 'Todo not found'
+			});
+			
+			// instead of responding with the todo, add it as a prop of an object,
+			// allows you to add further props to the returned obj in future without breaking the api
+			res.send({todo});
+			
+		}).catch((e) => res.status(400).send({
+			code: 400,
+			error: 'Bad request'
+		}));
+	}
+	
+});
+
 
 app.listen(3000, ()=>{
 	console.log('Express is listening on port 3000...');
