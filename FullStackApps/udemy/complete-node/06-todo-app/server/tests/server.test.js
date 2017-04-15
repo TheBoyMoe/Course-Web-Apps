098af47bb769333543dcac337f421f831a004a61
bill fero <billfero@gmail.com>
2017-04-15 11:14:38 +0100

    impl tests of the GET /todos/id route

--- FullStackApps/udemy/complete-node/06-todo-app/server/tests/server.test.js
+++ FullStackApps/udemy/complete-node/06-todo-app/server/tests/server.test.js
@@ -1,19 +1,30 @@
 "use strict";
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // seed data
 const todos = [
-	{text: 'First todo'},
-	{text: 'Second todo'},
-	{text: 'Third todo'}
+	{
+		_id: new ObjectID(),
+		text: 'First todo'
+	},
+	{
+		_id: new ObjectID(),
+		text: 'Second todo'
+	},
+	{
+		_id: new ObjectID(),
+		text: 'Third todo'
+	}
 ];
 
 
 // clear the db before every test case is run
+// use 'done' when it's an asynchronous test
 beforeEach((done)=>{
 	Todo.remove({}).then(()=>{
 		Todo.insertMany(todos);
@@ -81,4 +92,40 @@ describe('GET /todos', ()=>{
 			.end(done);
 	});
 	
+});
+
+
+// test GET /todos/:id route
+describe('GET /todos/:id', ()=>{
+
+	// fetch the first todo obj from the db using it's _id (db seeded using todos array)
+	it('Should return todo doc', (done)=>{
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`) // convert obj id to string
+			.expect(200)
+			.expect((res)=>{
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+	
+	// verify that a 404 is return when the id is not found
+	it('Should return 404 if todo not found', (done)=>{
+		// const id = todos[1]._id.toHexString();
+		const id = new ObjectID().toHexString();
+		request(app)
+			.get(`/todos/${id}`)
+			.expect(404)
+			.end(done);
+	});
+	
+	// verify that invalid id's return a 404
+	it('Should return 404 for invalid ids', (done)=>{
+		const id = 'fw09fw0fjw08fhjw0jfw0hjwefh';
+		request(app)
+			.get(`/todos/${id}`)
+			.expect(404)
+			.end(done);
+	});
+
 });
