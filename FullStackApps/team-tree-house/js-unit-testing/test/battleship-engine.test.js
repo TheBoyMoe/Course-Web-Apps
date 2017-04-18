547af4aaf5e20bbe1a79302b79b2994c7a9d9ced
bill fero <billfero@gmail.com>
2017-04-18 19:55:57 +0100

    setup files for battleship-engine tests

---
+++ FullStackApps/team-tree-house/js-unit-testing/test/battleship-engine.test.js
@@ -0,0 +1,11 @@
+'use strict';
+const mocha = require('mocha');
+const {expect} = require('chai');
+
+// test suite
+describe('Mocha', ()=>{
+	// unit tests - specs
+	it('should run our tests with npm', ()=>{
+		 expect(true).to.be.ok;
+	});
+});
