be1e27c787f665685f908ee6230ae9fecad72f47
bill fero <billfero@gmail.com>
2017-04-19 16:19:18 +0100

    initial commit - auth with expressand mongo - starter app

---
+++ FullStackApps/team-tree-house/express-auth/routes/index.js
@@ -0,0 +1,20 @@
+'use strict';
+let express = require('express');
+let router = express.Router();
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
