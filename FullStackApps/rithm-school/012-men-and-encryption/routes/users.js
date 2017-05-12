21d01fa63f03334c1c21bd34d9be74a2b937f93f
bill fero <billfero@gmail.com>
2017-05-12 17:31:12 +0100

    impl user routes

---
+++ FullStackApps/rithm-school/012-men-and-encryption/routes/users.js
@@ -0,0 +1,41 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const db = require('./../models/index');
+
+// GET /users
+router.get('/', (req, res, next)=>{
+    res.send('logged in!');
+});
+
+
+// GET /users/login
+router.get('/login', (req, res, next)=>{
+    res.render('login');
+});
+
+// GET /users/signup
+router.get('/signup', (req, res, next)=>{
+    res.render('new');
+});
+
+// POST /users/login
+router.post('/login', (req, res, next)=>{
+    db.User.findOne({username: req.body.username})
+        .then((user)=>{
+            user.comparePassword(req.body.password, (err, isMatch)=>{
+                if(isMatch) res.redirect('/users');
+                else res.redirect('/users/login');
+            })
+        }, (err) => res.send(err));
+});
+
+// POST /users/signup
+router.post('/signup', (req, res, next)=>{
+    db.User.create(req.body)
+        .then((user)=>{
+            res.redirect('/users/login');
+        });
+});
+
+module.exports = router;
