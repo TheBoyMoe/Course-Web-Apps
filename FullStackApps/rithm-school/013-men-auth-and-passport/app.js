c0e7b471972b7a591447a88ba149fd4f6062a3af
bill fero <billfero@gmail.com>
2017-05-16 18:08:41 +0100

    impl login and signup post routes incorporating passport

--- FullStackApps/rithm-school/013-men-auth-and-passport/app.js
+++ FullStackApps/rithm-school/013-men-auth-and-passport/app.js
@@ -1,3 +1,16 @@
+/*
+    Setting up passport:
+    1. select a strategy (how authentication is to be processed).
+        - Local Strategy  involves username and password
+        - External strategy - involves some 3rd party provider that will
+        provide the authentication service, e.g Facebook, Google, etc
+    2. create a new object from the strategy's constructor function,
+        passing in an optional configuration object(first parameter)
+    3. pass in a 2nd parameter which is a callback function(the verify callback)
+    4. if authentication is successful the user will be serialised - their info stored in the
+        session object, marking them as logged in and redirected to the successRoute.
+
+ */
 'use strict';
 require('dotenv').config({path: 'variables.env'});
 const express = require('express');
@@ -5,6 +18,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('cookie-session');
+
 
 // config app
 const app = express();
@@ -17,6 +33,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+// passport setup
+app.use(session({secret: process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // routes
 const userRoutes = require('./routes/users');
@@ -26,6 +47,10 @@ app.get('/', (req, res)=>{
     res.redirect('/users');
 });
 
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 
 // error handling
 app.use((req, res, next)=>{
--- FullStackApps/rithm-school/013-men-auth-and-passport/routes/users.js
+++ FullStackApps/rithm-school/013-men-auth-and-passport/routes/users.js
@@ -3,9 +3,44 @@ const express = require('express');
 const router = express.Router();
 const db = require('./../models/index');
 const auth = require('./../middleware/index');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+
+// passport local strategy
+passport.use(new LocalStrategy({
+    // configuration object, you must use the keys 'usernameField' and 'passwordField' when using a local strategy.
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true // default false, when true 1st param of verifyCallback will be req obj
+    
+}, function verifyCallback(req, username, password, done) {
+    // username and password fields are entered by user, done is a callback function
+    db.User.findOne({usename: username}, (err, user)=>{
+        if(err) return done(err);
+        if(!user) return done(null, false);
+        user.comparePassword(password, (error, isMatch)=>{
+            if(isMatch) return done(null, user);
+            return done(null, false);
+        })
+    })
+}));
+
+// only run if the verify callback returns a truthy value as the 2nd parameter,
+// the successCallback is run next
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    db.User.findById(id)
+        .then((user)=>{
+            done(null, user);
+        });
+});
 
 // GET /users
-router.get('/', (req, res)=>{
+router.get('/', auth.loginRequired, (req, res)=>{
     res.render('index');
 });
 
@@ -23,13 +58,28 @@ router.get('/signup', (req, res)=>{
 
 
 // POST /users/login
-
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/users',
+    failureRedirect: '/users/login'
+}));
 
 
 // POST /users/signup
-
+router.post('/signup', (req, res)=>{
+    db.User.create(req.body)
+        .then((user)=>{
+            req.login(user, (err)=>{
+                return res.redirect('/users');
+            })
+        })
+        .catch((err)=>next(err));
+});
 
 // GET /users/logout
-
+router.get('/logout', (req, res)=>{
+    req.logout();
+    req.flash('message', 'logged out!');
+    res.redirect('/users/login');
+});
 
 module.exports = router;
