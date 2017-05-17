7ba89f8b3b22f4bc952d6d3fcfcd5a5e7d00d61f
bill fero <billfero@gmail.com>
2017-05-17 09:25:21 +0100

    impl basic json api model

---
+++ FullStackApps/rithm-school/15-men-json-api/models/pet.js
@@ -0,0 +1,8 @@
+'use strict';
+const mongoose = require('mongoose');
+const petSchema = new mongoose.Schema({
+    name: String
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+module.exports = Pet;
---
+++ FullStackApps/rithm-school/15-men-json-api/routes/pets.js
@@ -0,0 +1,15 @@
+/*
+    // basic api routes
+    GET     /pets           show all pets
+    GET     /pets/:id       show a single pet
+    POST    /pets           create a pet
+    PATCH   /pets/:id       update a pet
+    DELETE  /pets/:id       delete a pet
+    
+    // http error codes
+    200     OK (for getting/updating)
+    201     Created (for creating)
+    204     No content (for deleting)
+    500     Internal server error (if something goes wrong at our end)
+ */
+'use strict';
