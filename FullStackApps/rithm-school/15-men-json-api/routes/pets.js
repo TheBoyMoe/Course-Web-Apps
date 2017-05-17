96cc1dda68c487528aa7eecad31729ba7318ac5d
bill fero <billfero@gmail.com>
2017-05-17 09:47:06 +0100

    impl routes

--- FullStackApps/rithm-school/15-men-json-api/routes/pets.js
+++ FullStackApps/rithm-school/15-men-json-api/routes/pets.js
@@ -13,3 +13,41 @@
     500     Internal server error (if something goes wrong at our end)
  */
 'use strict';
+const express = require('express');
+const router = express.Router();
+const db = require('./../models/index');
+
+// GET /pets
+router.get('/', (req, res, next)=>{
+    db.Pet.find()
+        .then((pets) => res.status(200).send(pets));
+});
+
+
+// GET /pets/:id
+router.get('/:id', (req, res, next)=>{
+    db.Pet.findById(req.params.id)
+        .then((pet) => res.status(200).send(pet));
+});
+
+// POST /pets
+router.post('/', (req, res, next)=>{
+    db.Pet.create(req.body)
+        .then((pet) => res.status(201).send(pet));
+});
+
+
+// PATCH /pets/:id
+router.patch('/:id', (req, res, next)=>{
+    db.Pet.findByIdAndUpdate(req.params.id)
+        .then((pet) => res.status(200).send(pet));
+});
+
+
+// DELETE /pets/:id
+router.delete('/:id', (req, res, next)=>{
+    db.Pet.findByIdAndRemove(req.params.id)
+        .then((pet) => res.status(204).send('Deleted item!'));
+});
+
+module.exports = router;
