f85ca2cfddb85682a251615ffc7dc9546d811497
bill fero <billfero@gmail.com>
2017-05-27 14:54:19 +0100

    define and use variables

---
+++ html-and-css/sass-basics/references.js
@@ -0,0 +1,25 @@
+/*
+	References:
+	[1] http://sass-lang.com/guide
+	[2] http://sass-lang.com/documentation/file.SASS_REFERENCE.html#variables_
+	[3] http://sass-lang.com/documentation/file.SASS_REFERENCE.html#syntax
+	[4] http://sass-lang.com/documentation/file.SASS_REFERENCE.html#using_sass
+	[5] http://sassbreak.com/ruby-sass-libsass-differences/
+	[6] http://sassbreak.com/watch-your-sass/
+	[7] https://www.sassmeister.com/
+	[8] https://codekitapp.com/
+	[9] http://scout-app.io/
+	[10] http://koala-app.com/
+	[11] https://coryetzkorn.com/blog/color-variables-in-sass/
+	[12] http://thesassway.com/beginner/variable-naming
+	
+	compiling single sass file to css:
+	$ sass input.scss:output.css
+	
+	you can have sass automatically update your sass files when any changes are saved with the watch command
+	$ sass --watch input.scss:output.css
+	
+	you can automatically combine multiple sass files into a single css file by watching the sass folder
+	$ sass --watch scss:css
+	
+ */
