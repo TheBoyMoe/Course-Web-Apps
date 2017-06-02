385c61742813ce22edc4e9a5dad0f833d74ef4da
bill fero <billfero@gmail.com>
2017-06-02 15:34:55 +0100

    combine express, templating and mongodb to create a basic app that displays a movie listing

--- FullStackApps/mongodb-uni/01-basic-app/app.js
+++ FullStackApps/mongodb-uni/01-basic-app/app.js
@@ -11,29 +11,23 @@ app.engine('html', consolidate.nunjucks);
 app.set('view engine', 'html'); // register nunjucks as the template engine
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
-	res.render('index', {'name': 'Templates'});
+// setup mongo client and the routes in the context of that connections db object
+MongoClient.connect('mongodb://localhost:27017/video', (err, db) => {
+	assert.equal(null, err);
+	console.log('Successfully connected to dbase');
+	
+	app.get('/', (req, res) => {
+		db.collection('movies').find({}).toArray((err, docs) => {
+			res.render('index', {'movies': docs});
+		});
+	});
+	
+	app.use((req, res) => {
+		res.sendStatus(404);
+	});
+	
+	app.listen(3000, () => {
+		console.log(`Express is listening on port ${port}`);
+	});
+	
 });
-
-app.use((req, res) => {
-	res.sendStatus(404);
-});
-
-app.listen(3000, () => {
-	console.log(`Express is listening on port ${port}`);
-});
-
-
-// MongoClient.connect('mongodb://localhost:27017/video', (err, db) => {
-// 	assert.equal(null, err);
-// 	console.log('Successfully connected to dbase');
-//
-// 	db.collection('movies').find({}).toArray((err, docs) => {
-// 		docs.forEach((doc) => console.log(doc.title));
-// 		db.close();
-// 	});
-// 	// due to asynchronous nature of node driver
-// 	// console message prints before doc titles
-// 	console.log('called find()');
-// });
-
